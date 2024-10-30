729c00028499d1eaddc6576153502171c7c0afd3
Ray Nimalan <raymondnimalan@gmail.com>
2024-10-30 19:35:52 -0400

    feat: add avoid keyboard wrapper to tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,42 +1,54 @@
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 
 type Props = {};
 
 const TabLayout: React.FC = () => {
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-            }}
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Adjust for iOS and Android
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0} // Adjust this value as necessary
         >
-            <Tabs.Screen
-                name='index'
-                options={{
-                    title: 'Play',
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabBarIcon
-                            name={focused ? 'home' : 'home-outline'}
-                            color={'#20b42acf6'}
-                        />
-                    ),
+            <Tabs
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: { position: 'absolute' },
+                    tabBarHideOnKeyboard: true,
                 }}
-            />
-            <Tabs.Screen
-                name='settings'
-                options={{
-                    title: 'Explore',
-                    tabBarIcon: ({ color, focused }) => (
-                        <TabBarIcon
-                            name={focused ? 'code-slash' : 'code-slash-outline'}
-                            color={color}
-                        />
-                    ),
-                }}
-            />
-        </Tabs>
+            >
+                <Tabs.Screen
+                    name='index'
+                    options={{
+                        title: 'Play',
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabBarIcon
+                                name={focused ? 'home' : 'home-outline'}
+                                color={'#20b42acf6'}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name='settings'
+                    options={{
+                        title: 'Explore',
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabBarIcon
+                                name={
+                                    focused
+                                        ? 'code-slash'
+                                        : 'code-slash-outline'
+                                }
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </KeyboardAvoidingView>
     );
 };
 
