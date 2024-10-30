441535b18fd838fd487609796a3b07e074faf8b9
Ray Nimalan <raymondnimalan@gmail.com>
2024-10-30 19:38:55 -0400

    feat: add drawer and button for found words

--- components/play/WordSearchScreen.tsx
+++ components/play/WordSearchScreen.tsx
@@ -1,17 +1,101 @@
 import WordSearch from '@/components/play/WordSearch';
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Button } from '@/components/ui/button';
+import {
+    Drawer,
+    DrawerBackdrop,
+    DrawerBody,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+} from '@/components/ui/drawer';
+import { GameContext } from '@/context/GameProvider';
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { ArrowRightCircle } from 'react-native-feather';
+import { wordSearchGame } from './wordSearchData';
+
+const words = wordSearchGame.words;
 
 const WordSearchScreen: React.FC = () => {
+    const [showDrawer, setShowDrawer] = useState(false);
+    const {
+        wordBank,
+        setWordBank,
+        foundWords,
+        setFoundWords,
+        notFoundWords,
+        setNotFoundWords,
+    } = useContext(GameContext);
+
+    useEffect(() => {
+        setWordBank(words);
+        setNotFoundWords(words);
+        console.log('word bank:', wordBank);
+    }, []);
+
+    useEffect(() => {
+        console.log('wordBank from screen', wordBank);
+    }, [wordBank]);
+    useEffect(() => {
+        console.log('foundWords from screen', foundWords);
+    }, [foundWords]);
+
     return (
-        <SafeAreaView
-            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-            <Text style={{ fontSize: 24, marginBottom: 20 }}>
-                Word Search Game
-            </Text>
-            <WordSearch />
-        </SafeAreaView>
+        <>
+            <View className='flex-1 justify-center items-center'>
+                <Text style={{ fontSize: 24, marginBottom: 20 }}>
+                    Word Search Game
+                </Text>
+                <Button className='border-2'>
+                    <ArrowRightCircle
+                        stroke='red'
+                        fill='#fff'
+                        width={32}
+                        height={32}
+                        onPress={() => {
+                            setShowDrawer(true);
+                        }}
+                    />
+                </Button>
+                <WordSearch />
+            </View>
+            <Drawer
+                isOpen={showDrawer}
+                onClose={() => {
+                    setShowDrawer(false);
+                }}
+                size='sm'
+                anchor='bottom'
+            >
+                <DrawerBackdrop />
+                <DrawerContent>
+                    <DrawerHeader>
+                        <Text>Words</Text>
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <View>
+                            {foundWords && foundWords.length !== 0 ? (
+                                foundWords.map((word, index) => (
+                                    <Text key={index}>{word}</Text>
+                                ))
+                            ) : (
+                                <Text>no words found</Text>
+                            )}
+                        </View>
+                    </DrawerBody>
+                    <DrawerFooter>
+                        <Button
+                            onPress={() => {
+                                setShowDrawer(false);
+                            }}
+                            className='flex-1'
+                        >
+                            <Text>Button</Text>
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
     );
 };
 
